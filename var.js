65743c21829c4664e35b3a1668181646546f996a
OMKAR CHAVAN <ovchavan3456@gmail.com>
2025-01-04 23:10:11 -0800

    Add files via upload

---
+++ var.js
@@ -0,0 +1,76 @@
+var test=12
+function foo(){
+    console.log(test);
+}
+foo();
+
+//Example 1
+var test1= 12,
+    test2= 20,
+    test3= 80
+    function foo(){
+        console.log(test1,test2,test3);
+    }
+    foo();  
+
+
+// Example 2 -take different numbers 
+    var test1= 12,
+    test2= 20,
+    test3= 80
+    function foo(){
+        console.log(test1,test2,test3);
+    }
+    foo();  
+
+
+//Example 3
+    var a= 10
+    var b= 80
+    var c= 80
+    var d= 90
+    var e= 50
+    var f= 10
+    var g= 60
+    var h= 40
+    var i= 40
+    var j= 30
+    function alpha(){
+        console.log(a,b,c,d,e,f,g,h,i,j);
+
+    }
+
+    alpha();
+
+// Example 4 to print the numbers in single line 
+    var a= 10
+    var b= 80
+    var c= 80
+    var d= 90
+    var e= 50
+    var f= 10
+    var g= 60
+    var h= 40
+    var i= 40
+    var j= 30
+    function Bita (){
+        console.log(a)
+        console.log(b)
+        console.log(c)
+        console.log(d)
+        console.log(e)
+        console.log(f)
+        console.log(g)
+        console.log(h)
+        console.log(i)
+        console.log(j)
+
+    }
+
+    Bita();
+ 
+
+
+    
+
+
